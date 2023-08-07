1b03a1ac6d6afbd0631a0001eab07da980470f1e
Rishit Bhowmick <138646591+RB4568@users.noreply.github.com>
2023-08-07 09:39:18 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+
+main()
+{
+    int a=10;
+    char name[10]="rishit";
+    
+    cout<<"name="<<name<<endl;
+    cout<<"id no:"<<a;
+}
---
+++ sum.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class r
+{
+    public:	void bhk()
+	{
+		int a,b,sum;
+	cout<<"enter the value of a: ";
+	cin>>a;
+	cout<<endl;
+	cout<<"enter the value of b: ";
+	cin>>b;
+	cout<<endl;
+	sum=a+b;
+		cout<<"addition of a and b:"<<sum;
+	}
+	
+};
+main()
+{
+	r obj;
+	
+	obj.bhk();
+	
+}
